0ffe9301f9c0d4db8015cff51ae406f22aa65571
Arjun Vasavan <vasavanarjun@gmail.com>
2026-02-10 08:38:03 +0530

    print func and reading arg compltd

--- include/apc.h
+++ include/apc.h
@@ -3,5 +3,21 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+
+typedef struct Dlist{  // NOTE: in typedef we can put name early also
+    int data;
+    struct Dlist* prev;
+    struct Dlist* next;
+}Dlist;
+
+typedef enum {
+    SUCCESS, // 0
+    FAILURE // 1
+}status;
+
+
+status create_list(char* argv, Dlist** head, Dlist** tail);
+void print_list(Dlist* head,Dlist* tail);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,4 +1,41 @@
 #include "../include/apc.h"
 int main(int argc, char** argv) {
-    printf("Batman\n");
+    // argument should be ./a.out 100 + 200
+    printf("Argument read is %s %s %s\n",argv[1],argv[2],argv[3]);
+
+    // TODO: validation of input argument
+
+    Dlist* head_1 = NULL;
+    Dlist* tail_1 = NULL;
+    Dlist* head_2 = NULL;
+    Dlist* tail_2 = NULL;
+    Dlist* head_R = NULL;
+    Dlist* tail_R = NULL;
+
+    char operator = argv[2][0];
+
+    create_list(argv[1],&head_1,&tail_1);
+    print_list(head_1,tail_1);
+    create_list(argv[3],&head_2,&tail_2);
+    print_list(head_2,tail_2);
+
+    switch (operator) {
+        case '+':{
+            break;
+        }
+        case '-':{
+            break;
+        }
+        case '*':{
+            break;
+        }
+        case '/':{
+            break;
+        }
+        default:{
+            printf("Error Enter valid operation\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
 }
